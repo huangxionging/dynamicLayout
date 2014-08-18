55043320b3d0da8076dc55f232ea12a9a970cd09
huangxiong <huangxiong@huangxiong.local>
2014-08-18 18:43:02 +0800

    Initial Commit

---
+++ dynamicLayout/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  dynamicLayout
+//
+//  Created by huangxiong on 14-8-18.
+//  Copyright (c) 2014年 New-Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
